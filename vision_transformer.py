bd0719681b908e8a9a66346d0bbfcd9805d99563
Quang Anh <quanganhvp32401@gmail.com>
2023-06-02 04:22:31 +0700

    u

--- vision_transformer.py
+++ vision_transformer.py
@@ -521,12 +521,12 @@ class VisionTransformer(nn.Module):
     #change task_id from -1 to 0
     def forward_features(self, x, task_id=-1, cls_features=None, train=False):
         x = self.patch_embed(x)
-
-        #add genertor prompt
-        self.generator_prompt = promptGe(length=1, embed_dim=768)
-        print("task_id", task_id)
-        res_ge = self.generator_prompt(x, task_id)
-        x = res_ge['prompted_embedding']
+        if task_id >= 0:
+            #add genertor prompt
+            self.generator_prompt = promptGe(length=1, embed_dim=768)
+            print("task_id", task_id)
+            res_ge = self.generator_prompt(x, task_id)
+            x = res_ge['prompted_embedding']
         
         if self.cls_token is not None:
             x = torch.cat((self.cls_token.expand(x.shape[0], -1, -1), x), dim=1)
