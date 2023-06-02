18d0f89e4bd3c072e7b620b99a6dafb2905b0ecb
Quang Anh <quanganhvp32401@gmail.com>
2023-06-02 13:42:15 +0700

    u

--- GePrompt.py
+++ GePrompt.py
@@ -14,8 +14,10 @@ class promptGe(nn.Module):
         #Generation network
         self.generation_layer_1 = nn.Linear(768, 256, bias=True)
         self.generation_layer_1.cuda()
+
         self.generation_activation = nn.ReLU()
         self.generation_activation.cuda()
+        
         self.generation_layer_2 = nn.Linear(256, 768*5, bias=True)
         self.generation_layer_2.cuda()
 
@@ -27,17 +29,15 @@ class promptGe(nn.Module):
         n = self.task_embed_layer(m).to(x_embed.device)
 
         x_task_embed = torch.cat((x_embed, n), dim=1)
-        a = self.generation_layer_1(x_task_embed)
+        x_task_embed_mean = x_task_embed.mean(dim=1).unsqueeze(1)
+        a = self.generation_layer_1(x_task_embed_mean)
         b = self.generation_activation(a)
-        batched_prompt_raw = self.generation_layer_2(b)     # B, length, C * 5
-
-        # prompt_1 = batched_prompt_raw[:, :, :768]
-        # prompt_2 = batched_prompt_raw[:, :, 768:768*2]
-        # prompt_3 = batched_prompt_raw[:, :, 768*2:768*3]
-        # prompt_4 = batched_prompt_raw[:, :, 768*3:768*4]
-        # prompt_5 = batched_prompt_raw[:, :, 768*4:768*5]
+        batched_prompt_raw = self.generation_layer_2(b)     # B, 1, C * 5
+        batched_prompt_raw = batched_prompt_raw.reshape((16, 5, 768))
 
         out['total_prompt_len'] = 5
         out['prompted_embedding'] = torch.cat([batched_prompt_raw, x_embed], dim=1)
         return out
         
+
+# Batch , number of prompts, Embedding
