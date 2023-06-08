bf31cf79bac0dd7863beae0d4a8ed45d30ad0636
CryingBI <quanganhvp32401@gmail.com>
2023-06-08 15:32:47 +0700

    update test 2 3

--- vision_transformer.py
+++ vision_transformer.py
@@ -585,6 +585,7 @@ class VisionTransformer(nn.Module):
                         if self.use_prefix_tune_for_e_prompt:
                             # Prefix tunning, [B, 2, top_k * e_prompt_length, num_heads, embed_dim // num_heads]
                             idx = torch.tensor([e_prompt_counter] * x.shape[0]).to(x.device)
+                            task_id = torch.tensor([task_id]).to(x.device)
                             e_prompt_new = self.e_prompt_all_task[task_id][idx] 
                             x = block(x, prompt=e_prompt_new)
                         
