7f5ae8be206eec8cf66f321c199fef8c88669db6
CryingBI <quanganhvp32401@gmail.com>
2023-06-08 15:47:51 +0700

    print .device

--- vision_transformer.py
+++ vision_transformer.py
@@ -586,6 +586,9 @@ class VisionTransformer(nn.Module):
                             # Prefix tunning, [B, 2, top_k * e_prompt_length, num_heads, embed_dim // num_heads]
                             idx = torch.tensor([e_prompt_counter] * x.shape[0]).to(x.device)
                             task_id = torch.tensor([task_id]).to(x.device)
+                            print(x.device)
+                            print(idx.device)
+                            print(task_id.device)
                             e_prompt_new = self.e_prompt_all_task[task_id][idx] 
                             x = block(x, prompt=e_prompt_new)
                         
