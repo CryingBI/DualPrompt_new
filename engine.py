ccfa16798ee7256ab2bf84964c47d5c2989928bc
Quang Anh <quanganhvp32401@gmail.com>
2023-06-09 21:57:00 +0700

    u

--- engine.py
+++ engine.py
@@ -389,7 +389,8 @@ def sample_data(model: torch.nn.Module, data_loader, device,
         x_encoded = torch.cat(x_encoded, dim=0)
         x_encoded = x_encoded.reshape((x_encoded.shape[0], x_encoded.shape[1] * x_encoded.shape[2]))
         print(x_encoded.shape)
-        gm = GaussianMixture(n_components=5, random_state=0).fit(x_encoded.cpu().detach().numpy())
+        gm = GaussianMixture(n_components=5, random_state=0).fit(x_encoded)
+        print("OK")
         gm_list.append(gm)
 
 
