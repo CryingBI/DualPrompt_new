04b1692d92faa87960fcac721a46427ce9224788
Quang Anh <quanganhvp32401@gmail.com>
2023-06-10 13:05:53 +0700

    u

--- engine.py
+++ engine.py
@@ -421,9 +421,16 @@ def evaluate_new(model: torch.nn.Module, task_model: torch.nn.Module, data_loade
 
             output = model.forward_features(input, task_id)
             logits = task_model(output['x'])
+            print(f"logits: {logits.shape}")
+
             prob = F.softmax(logits, dim = 1)
+            print(f"prob: {prob.shape}")
+
             task_id_infer = torch.argmax(prob)
+            print(f"task_id_infer: {task_id_infer.shape}")
+
             last_logits = model(input, task_id_infer)
+            print(f"last_logits: {last_logits.shape}")
 
             loss = criterion(last_logits, target)
 
