58fd29dc0862ed6f502832e583db13936fe879b0
Quang Anh <quanganhvp32401@gmail.com>
2023-06-26 09:47:34 +0700

    u

--- head_model.py
+++ head_model.py
@@ -5,18 +5,18 @@ import torch.nn as nn
 class TaskClassifier(nn.Module):
     def __init__(self, args):
         super().__init__()
-        # self.classifier = nn.Sequential(nn.Linear(768, 512), 
-        #                                 nn.ReLU(inplace=True), 
-        #                                 nn.Linear(512, 256),
-        #                                 nn.ReLU(inplace=True),
-        #                                 nn.Linear(256, 256),
-        #                                 nn.ReLU(inplace=True),
-        #                                 nn.Linear(256, 10)).cuda()
-        
-        self.classifier = nn.Sequential(nn.Linear(768, 384), 
+        self.classifier = nn.Sequential(nn.Linear(768, 512), 
                                         nn.ReLU(inplace=True), 
-                                        nn.Linear(384, 384),
+                                        nn.Linear(512, 256),
                                         nn.ReLU(inplace=True),
-                                        nn.Linear(384, 10)).cuda()
+                                        nn.Linear(256, 256),
+                                        nn.ReLU(inplace=True),
+                                        nn.Linear(256, 10)).cuda()
+        
+        # self.classifier = nn.Sequential(nn.Linear(768, 384), 
+        #                                 nn.ReLU(inplace=True), 
+        #                                 nn.Linear(384, 384),
+        #                                 nn.ReLU(inplace=True),
+        #                                 nn.Linear(384, 10)).cuda()
     def forward(self, x):
         return self.classifier(x)
