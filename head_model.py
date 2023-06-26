202feab041174708c6a0791035c782aeae00b319
Quang Anh <quanganhvp32401@gmail.com>
2023-06-26 12:55:36 +0700

    u

--- head_model.py
+++ head_model.py
@@ -6,11 +6,14 @@ class TaskClassifier(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.classifier = nn.Sequential(nn.Linear(768, 512), 
-                                        nn.ReLU(inplace=True), 
+                                        nn.ReLU(inplace=True),
+                                        nn.Dropout(0.5), 
                                         nn.Linear(512, 256),
                                         nn.ReLU(inplace=True),
+                                        nn.Dropout(0.5),
                                         nn.Linear(256, 256),
                                         nn.ReLU(inplace=True),
+                                        nn.Dropout(0.5),
                                         nn.Linear(256, 10)).cuda()
         
         # self.classifier = nn.Sequential(nn.Linear(768, 384), 
