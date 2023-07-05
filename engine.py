8dc064643a486c47a98dd367493dc4342029d0b0
Quang Anh <quanganhvp32401@gmail.com>
2023-07-05 19:30:32 +0700

    u

--- engine.py
+++ engine.py
@@ -280,7 +280,7 @@ def train_task_model(task_model: torch.nn.Module, device, gm_list, epochs, task_
     lr = 5e-5
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(task_model.parameters(), lr=lr)
-    scheduler = lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=0.5, total_iters=90)
+    scheduler = lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=0.8, total_iters=90)
 
     gm_use = gm_list[:10*(task_id+1)]
     input_train = []
