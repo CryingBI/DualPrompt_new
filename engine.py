b0deb470a4b1002ae1dd5cfd2aa56a8940994e41
Quang Anh <quanganhvp32401@gmail.com>
2023-07-27 20:11:16 +0700

    u

--- engine.py
+++ engine.py
@@ -277,13 +277,13 @@ def train_task_model(task_model: torch.nn.Module, device, gm_list, epochs, args,
 
     #training_data = [[] for e_id in range(epochs)]
     data_loader_data = []
-    lr = 7e-5
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(task_model.parameters(), lr=lr)
     scheduler = lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=0.005, total_iters=90)
 
     if args.dataset == "Split-CIFAR100":
         gm_use = gm_list[:10*(task_id+1)]
+        lr = 7e-5
         input_train = []
         target_train = []
         for i in range(len(gm_use)):
@@ -358,6 +358,7 @@ def train_task_model(task_model: torch.nn.Module, device, gm_list, epochs, args,
 
     else:
         gm_use = gm_list[:(task_id+1)]
+        lr = 1e-3
         input_train = []
         target_train = []
         print("len gm", len(gm_use))
